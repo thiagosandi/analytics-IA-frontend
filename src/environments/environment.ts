7fdf185633601e0fa24cc3e8748e658e6d927357
Thiago Sandi <thiago.sandi91@gmail.com>
2025-12-31 00:36:34 -0300

    add environment

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDVnEi4fxwa_zWcNBjTydyWZglZp2lmgwE",
+    authDomain: "miniblog-9aaba.firebaseapp.com",
+    projectId: "miniblog-9aaba",
+    storageBucket: "miniblog-9aaba.firebasestorage.app",
+    messagingSenderId: "80966420498",
+    appId: "1:80966420498:web:2e5654d600e1a812b070cb"
+  }
+};
